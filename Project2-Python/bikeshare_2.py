f09f2ec04210d94b8ece3d16dcadfa92b449bf03
Jon Sweeney <jonsweeney26@gmail.com>
2023-07-09 09:10:30 -0700

    clean up greeting

--- Project2-Python/bikeshare_2.py
+++ Project2-Python/bikeshare_2.py
@@ -1,4 +1,5 @@
 import math
+import os
 import time
 import pandas as pd
 import numpy as np
@@ -84,6 +85,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+    os.system('clear')
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = get_city()
